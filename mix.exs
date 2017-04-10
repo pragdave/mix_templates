186119e03170ff4c554f6cb91479f874bc4f12e0
pragdave <dave@pragdave.me>
2017-04-10 13:21:45 -0500

    initial load—do not use

--- mix.exs
+++ mix.exs
@@ -4,18 +4,20 @@ defmodule MixTemplates.Mixfile do
   @name    :mix_templates
   @version "0.1.0"
   @deps [
-    private: "> 0.0.0"
-    ex_doc:  [ "~> 0.14", only: :dev, runtime: false ],
+    { :private, "> 0.0.0" },
+    { :ex_doc,  "~> 0.14", only: :dev, runtime: false },
   ]
 
   @description """
   A modular, open templating system. It's designed for use with 
   `mix gen`, but could probably be used elsewhere. 
 
-  You care about this if ① you'd like to use project templates that
-  are the ones built in to mix, ② you'd like to create your own
-  templates, or ③ you have created a package such as Phoenix or Nerves
-  that need their own project setup.
+  You care about this if:
+
+  ① you'd like to use project templates other than the ones built in to mix, 
+  ② you'd like to create your own templates, or 
+  ③ you have created a package such as Phoenix or Nerves that need their 
+     own project setup.
   """
   
   ############################################################
@@ -51,5 +53,4 @@ defmodule MixTemplates.Mixfile do
       }
     ]
   end
-  
 end
